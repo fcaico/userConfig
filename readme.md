02900e342e88a666619ff3d3b2dd0e29d0d8d521
Frank Caico <fcaico@gmail.com>
2020-05-20 12:29:56 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+## What is this?
+This repo's purpose is to help quickly setup my shell environment, when restarting or when getting a new machine.
+
+- It serves as a backup of all my '.'files
+- It serves as a backup for crafter cli helper shell scripts
+
+## Assumptions
+- Intended to be used in 'Xnix' environments.
+- This is based on zsh and oh-my-zsh
+
+## Usage
+
+In your favorite terminal window and run theses commands:
+
+```bash
+$ cd ~
+$ git clone https@github.com:fcaico/userConfig.git .userConfig
+$ cd .userConfig && ./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env zsh
+
+mydir=${(%):-%N}
+mydir=`dirname $mydir`
+mydir=${0:a:h}
+echo $mydir
+
+function setup_ohmyzsh {
+  destination="${1}"
+  if [[ ! -d "${destination}/.oh-my-zsh" ]]; then
+	echo "Cloning oh-my-zsh from robbyrussel"
+  	(cd "${destination}" && git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh)
+  else
+	echo "oh-my-zsh already exists.  Updating."
+  	(cd "${destination}/.oh-my-zsh" && git pull origin master)
+  fi
+}
+
+function setup_dotfiles ()
+{
+  dotfile_location="${1}"
+  dotfiles=(`ls $dotfile_location`)
+  destination="${2}"
+
+  for dotfile in "${dotfiles[@]}"
+  do
+    fullpath_dest="${destination}"/."${dotfile}"
+    fullpath_src="${dotfile_location}/${dotfile}"
+
+    [[ -L "${fullpath_dest}" ]] && rm "${fullpath_dest}";
+    if [[ -f "${fullpath_src}" ]]; then
+      [[ -f "${fullpath_dest}" ]] && rm "${fullpath_dest}"
+    fi
+
+    ln -s "${fullpath_src}" "${fullpath_dest}"
+	echo ".${dotfile} linked."
+
+  done
+}
+
+echo "\nSetting up oh-my-zsh\n"
+setup_ohmyzsh ${mydir}
+
+echo "\nSetting up dotfiles\n"
+setup_dotfiles ${mydir}/dotfiles ${mydir}/..
